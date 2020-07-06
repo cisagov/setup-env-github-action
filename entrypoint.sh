b753f25faf8d1160799fee76b93240c0d7dfc9fa
Felddy <markf@geekpad.com>
2020-07-06 13:30:05 -0400

    Bump Terraform and Packer to latest releases.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "::set-env name=PACKER_VERSION::1.5.6"
-echo "::set-env name=TERRAFORM_VERSION::0.12.26"
+echo "::set-env name=PACKER_VERSION::1.6.0"
+echo "::set-env name=TERRAFORM_VERSION::0.12.28"
