da4714081aee732161162cd4b8992d6d97a4cf85
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2021-09-20 10:52:09 -0400

    Store versions separately
    Store the program versions separately from where they
    are output as environment variables.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+packer_version=1.7.3
+shfmt_version=v3.3.0
+terraform_version=0.14.11
+terraform_docs_version=v0.14.1
+
 {
-  echo "PACKER_VERSION=1.7.3"
-  echo "SHFMT_VERSION=v3.3.0"
-  echo "TERRAFORM_VERSION=0.14.11"
-  echo "TERRAFORM_DOCS_VERSION=v0.14.1"
+  echo "PACKER_VERSION=$packer_version"
+  echo "SHFMT_VERSION=$shfmt_version"
+  echo "TERRAFORM_VERSION=$terraform_version"
+  echo "TERRAFORM_DOCS_VERSION=$terraform_docs_version"
 } >> "$GITHUB_ENV"
