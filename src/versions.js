dd85c6579a01b73bfa001acbf60f5ad0f56c2b3f
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2026-06-10 16:41:25 -0400

    Bump goimports from v0.42.0 to v0.45.0

--- src/versions.js
+++ src/versions.js
@@ -8,7 +8,7 @@ const toolVersions = {
   go: "1.26",
   "go-critic": "v0.14.3",
   "go-junit-report": "v2.1.0",
-  goimports: "v0.42.0",
+  goimports: "v0.45.0",
   gomock: "v1.6.0",
   gosec: "v2.24.0",
   /**
